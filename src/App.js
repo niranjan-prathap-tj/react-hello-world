7debb6f051acf40eb637f8f6486d4b4b42a11001
niranjan-prathap-tj <niranjan.prathap@ideas2it.com>
2024-07-18 14:58:06 +0530

    init-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [fastApiData, setFastApiData] = useState([]);
+  const [djangoData, setDjangoData] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch data from FastAPI backend
+    const fetchFastApiData = async () => {
+      try {
+        const response = await fetch('https://34.93.215.136/fastapi');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data from FastAPI');
+        }
+        const data = await response.json();
+        setFastApiData(data.message); // Updating state with fetched data
+      } catch (error) {
+        console.error('Error fetching data from FastAPI:', error);
+      }
+    };
+
+    // Function to fetch data from Django backend
+    const fetchDjangoData = async () => {
+      try {
+        const response = await fetch('https://34.93.215.136/django/test/');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data from Django');
+        }
+        const data = await response.text();
+        setDjangoData(data); // Updating state with fetched data
+      } catch (error) {
+        console.error('Error fetching data from Django:', error);
+      }
+    };
+
+    // Call the fetch functions on component mount
+    fetchFastApiData();
+    fetchDjangoData();
+  }, []); // Empty dependency array to run once on component mount
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-center">React App Calling FastAPI and Django</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <h2>Data from FastAPI:</h2>
+              <ul>
+                {fastApiData}
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <h2>Data from Django:</h2>
+              <ul>
+                {djangoData}
+              </ul>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
